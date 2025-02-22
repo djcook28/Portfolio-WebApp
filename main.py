9bd4a65ee33abd408ec36e8365d59ed82ae33ed8
dcook <daniel.j.cook.28@gmail.com>
2025-02-22 14:53:18 -0600

    Added 'mock company website app' fixed col division so that app accurately split between the 2 columns

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import streamlit as st
-import csv
+import pandas
 from PIL import Image
 
-#col1, col2 = st.columns(2)
+st.set_page_config(layout='wide')
 
-appDict = csv.DictReader(open('appTable.csv'))
+app_dict = pandas.read_csv('appTable.csv',sep=",")
+app_dict_len = len(app_dict)
 
 st.title('Home')
+st.write("Below you will find some Python Apps that I built.  Feel free to contact me.")
 
-for app in appDict:
-    st.subheader(app['Title'])
-    st.write(app['Description'])
-    app_image = Image.open(f"images/{app['image']}")
-    st.image(app_image)
-    st.link_button(label=f'{app['Title']}', url=app['URL'])
+col1, empty_col, col2 = st.columns([2, 0.5, 2])
+col_len = int(app_dict_len / 2)
+
+with col1:
+    for int, app in app_dict[:col_len].iterrows():
+        st.subheader(app['Title'])
+        st.write(app['Description'])
+        st.image(f"images/{app['image']}")
+        #you can either have a URL as text as below or a link button
+        # st.link_button(label=f'{app['Title']} Source Code', url=app['URL'])
+        st.write(f"[Source Code]({app['URL']})")
+
+
+with col2:
+    for int, app in app_dict[col_len:].iterrows():
+        st.subheader(app['Title'])
+        st.write(app['Description'])
+        st.image(f"images/{app['image']}")
+        #st.link_button(label=f'{app['Title']} Source Code', url=app['URL'])
+        st.write(f"[Source Code]({app['URL']})")
